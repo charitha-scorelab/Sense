29e075935ba918f322c8536ae3732d88dcc1be8a
charitha Madusanka <charitha@scorelab.org>
2013-10-22 18:07:42 +0530

    Sense service implementation begins.

---
+++ src/org/scorelab/sense/Sense.java
@@ -0,0 +1,14 @@
+package org.scorelab.sense;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class Sense extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return null;
+	}
+
+}
